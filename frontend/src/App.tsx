1052f9995df3682372ba3c40d0d1f670edb34cd9
Arthur Naudy <arthur.naudy@student-cs.fr>
2021-11-23 17:05:31 +0100

    Table to show available data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,13 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Axios from "axios";
 
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 
 function App() {
@@ -15,15 +22,47 @@ function App() {
   },[]);
 
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <>
+      <div className="App">
+        <h1>Hello</h1>
 
-      { dataList.map((val: any) => {
-          return <h1>{val.id}</h1>
-        })
-      }
+        { dataList.map((val: any) => {
+            return <h1>{val.id}</h1>
+          })
+        }
 
-    </div>
+      </div>
+
+      <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {dataList.map((val: any) => (
+            <TableRow
+              key={val.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {val.id}
+              </TableCell>
+              <TableCell align="right">{}</TableCell>
+              <TableCell align="right">{}</TableCell>
+              <TableCell align="right">{}</TableCell>
+              <TableCell align="right">{}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      </TableContainer>
+    </>
   );
 }
 
