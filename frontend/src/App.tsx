c579c8efe25aa7bd9d497584c888b85398625f1d
Arthur Naudy <arthur.naudy@student-cs.fr>
2021-11-24 10:23:02 +0100

    display daily events of one patient in a table

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,26 +10,48 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
+import date from 'date-and-time';
 
 function App() {
 
-  const [dataList, setDataList] = useState([]);
+  // const [dataList, setDataList] = useState([]);
+  // const [timeList, setTimeList] = useState([]);
+  const [dailyList, setDailyList] = useState([]);
+
+
+  // useEffect(() => {
+  //   Axios.get("http://localhost:8000/test").then((response) => {
+  //     setDataList(response.data);
+  //   });
+  // },[]);
+  
+  // useEffect(() => {
+  //   Axios.get("http://localhost:8000/timestamps").then((response) => {
+  //     setTimeList(response.data);
+  //   });
+  // },[]);
 
   useEffect(() => {
-    Axios.get("http://localhost:8000/test").then((response) => {
-      setDataList(response.data);
+    Axios.get("http://localhost:8000/daily").then((response) => {
+      setDailyList(response.data);
     });
   },[]);
 
   return (
     <>
       <div className="App">
-        <h1>Hello</h1>
+        <h1>Daily events of a patient</h1>
+
+        {/* { timeList.map((val: any) => {          
+          const value = date.format((new Date(val.timestamp)),'YYYY/MM/DD HH:mm:ss');
+            return <h1>{value}</h1>
+          })
+        }
 
         { dataList.map((val: any) => {
             return <h1>{val.id}</h1>
           })
-        }
+        } */}
 
       </div>
 
@@ -37,26 +59,20 @@ function App() {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Time</TableCell>
+            <TableCell align="right">Event</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {dataList.map((val: any) => (
+          {dailyList.map((val: any) => (
             <TableRow
               key={val.id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {val.id}
+                {val.timestamp}
               </TableCell>
-              <TableCell align="right">{}</TableCell>
-              <TableCell align="right">{}</TableCell>
-              <TableCell align="right">{}</TableCell>
-              <TableCell align="right">{}</TableCell>
+              <TableCell align="right">{val.event_type}</TableCell>
             </TableRow>
           ))}
         </TableBody>
