dc8e651523e2629e74bd6ab5e8f1b2b092abccdf
Arthur Naudy <arthur.naudy@student-cs.fr>
2021-11-24 10:25:58 +0100

    daily event query in backend

--- backend/src/application.ts
+++ backend/src/application.ts
@@ -29,6 +29,24 @@ app.get("/test", async(_, res) => {
     res.send(data);
 });
 
+app.get("/care_recipients", async(_, res) => {
+  const data = await sequelize.query("SELECT care_recipient_id FROM birdietest.events GROUP BY  care_recipient_id ", { type: QueryTypes.SELECT });
+  console.log('data', data);
+  res.send(data);
+});
+
+app.get("/timestamps", async(_, res) => {
+  const data = await sequelize.query("SELECT timestamp FROM birdietest.events WHERE event_type = 'task_completed' AND  caregiver_id = '5c9090ab-7d5e-4a72-8bf7-197190ad4c98' AND task_instance_id = 'dHxmMjU2YmFlYS1jODEyLTRjZWMtOTUxNC0wYzc5YjNjZmQwMzN8MjAxOS0wNS0xMlQwNzowMDowMC4wMDBafE1PUk5JTkc=' ", { type: QueryTypes.SELECT });
+  console.log('data', data);
+  res.send(data);
+});
+
+app.get("/daily", async(_,res) => {
+  const data = await sequelize.query("SELECT id, timestamp, event_type FROM birdietest.events WHERE DATE_FORMAT(timestamp, '%d/%m/%Y') = '23/04/2019' AND care_recipient_id = 'ad3512a6-91b1-4d7d-a005-6f8764dd0111' ORDER BY TIMESTAMP(timestamp)")
+  console.log('data', data[0]);
+  res.send(data[0]);
+});
+
 app.use(pingController); 
 
 export default app;
